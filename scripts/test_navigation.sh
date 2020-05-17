78ef8a397475793d1d77369f9ecd4992714b8ec9
Ytodi31 <yashaarth@todi89.com>
2020-05-17 12:49:46 -0400

    feat : Add shell script to test amcl localisation

---
+++ scripts/test_navigation.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find home_service)/worlds/myworld.world" &
+sleep 8
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 8
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 5
