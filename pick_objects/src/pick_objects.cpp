51ea24f360a6c9d568e9c1939a8c9be68ad8b863
Ytodi31 <yashaarth@todi89.com>
2020-05-18 03:17:57 -0400

    feat : Add pickup and drop command for navigation

--- pick_objects/src/pick_objects.cpp
+++ pick_objects/src/pick_objects.cpp
@@ -23,8 +23,9 @@ int main(int argc, char** argv){
  goal.target_pose.header.stamp = ros::Time::now();
 
  // Define a position and orientation for the robot to reach
- goal.target_pose.pose.position.x = 7.46;
-  goal.target_pose.pose.position.y = -5.06;
+
+  goal.target_pose.pose.position.x = -5.25;
+  goal.target_pose.pose.position.y = 6.2;
 // goal.target_pose.pose.position.x = 1;
  goal.target_pose.pose.orientation.w = 1.0;
 
@@ -37,23 +38,23 @@ int main(int argc, char** argv){
    ROS_INFO("Hooray, the robot reached the pickup location");
  else
    ROS_INFO("The robot has failed to reach the pickup location");
-  //
-  // ros::Duration(5).sleep();
-  // // Define a position and orientation for the robot to reach
-  // goal.target_pose.pose.position.x = -5.25;
-  // goal.target_pose.pose.position.y = 7.87;
-  // goal.target_pose.pose.orientation.w = 1.0;
-  //
-  //
-  //  ROS_INFO("Sending goal");
-  //  ac.sendGoal(goal);
-  //
-  //  ac.waitForResult();
-  //
-  //  if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-  //    ROS_INFO("Hooray, the robot reached the pickup location");
-  //  else
-  //    ROS_INFO("The robot has failed to reach the pickup location");
 
+  ros::Duration(5).sleep();
+  // Define a position and orientation for the robot to reach
+  goal.target_pose.pose.position.x = 7.7;
+   goal.target_pose.pose.position.y = -3.0;
+  goal.target_pose.pose.orientation.w = 1.0;
+
+
+   ROS_INFO("Sending goal");
+   ac.sendGoal(goal);
+
+   ac.waitForResult();
+
+   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
+     ROS_INFO("Hooray, the robot reached the pickup location");
+   else
+     ROS_INFO("The robot has failed to reach the pickup location");
+  ros::Duration(1).sleep();
   return 0;
 }
