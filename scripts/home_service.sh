f8ff368c9a50fbd12be5e1c0522ecd4e6ea7a68e
Ytodi31 <yashaarth@todi89.com>
2020-05-18 14:10:49 -0400

    feat : Add shell script to launch home_service robot with marker and amcl

---
+++ scripts/home_service.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/ytodi31/my_ws/src/map/map.yaml
+xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find home_service)/worlds/myworld.world" &
+sleep 8
+xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
+sleep 8
+xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
+sleep 8
+xterm -e "rosrun add_markers home_service" &
+sleep 8
+xterm -e "rosrun pick_objects pick_objects" &
+sleep 5
