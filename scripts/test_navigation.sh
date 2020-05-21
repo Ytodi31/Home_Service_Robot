4bbb72b395f392f2a76082a47dd2fef52f7d8216
Ytodi31 <yashaarth@todi89.com>
2020-05-21 19:15:55 -0400

    feat : add map of custom world and remove turtlebot default world

--- scripts/test_navigation.sh
+++ scripts/test_navigation.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
+export TURTLEBOT_GAZEBO_MAP_FILE=/home/ytodi31/my_ws/src/map/map.yaml
 xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=$(rospack find home_service)/worlds/myworld.world" &
 sleep 8
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 8
 xterm -e "roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 5
+xterm -e "roslaunch turtlebot_teleop keyboard_teleop.launch"
